a94ef849b30a25fe12abc9307f38aa02be17e32e
BroVic <victorordu@outlook.com>
2016-05-21 13:24:00 +0100

    Updates the file

--- sandbox.R
+++ sandbox.R
@@ -25,28 +25,3 @@ physical.threat <- table(mydata$concern.bullying, mydata$concern.violence)
 barplot(physical.threat, beside = TRUE, legend = TRUE, xlab = "Bullying",
         legend.text = TRUE)
 chisq.test(physical.threat)
-
-# Fresh plotting approach
-# Plotting function
-multiplot <- function(x, y) {
-  layout(matrix(c(1:3, 4:6, 7, 7, 7), nrow = 3, byrow = TRUE))
-  par(mar = c(0, 3, 5, 1))
-  col <- c("green", "yellow", "red")
-  for (i in y) {
-    ht <- table(x, mydata[, i])
-    barplot(ht,
-            beside = TRUE,
-            legend = FALSE,
-            ylim = c(0, max(ht)),
-            yaxt = "s",
-            col = c("green", "yellow", "red"),
-            xlab = colnames(mydata[i]))
-  }
-  plot(1, type = "n", axes = FALSE, ylab = "")
-  legend("top", inset = 0, legend = levels(x),
-         horiz = FALSE, fill = col, col = col,
-         title = attr(x, which = "name"), bty = "n")
-  layout(matrix(1))
-  oldpar <- par()
-}
-
