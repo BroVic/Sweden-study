60bc6e26638a8ca0d9e6912dfcf7cfe283406d03
BroVic <victorordu@outlook.com>
2016-06-03 15:15:44 +0100

    Updates sandbox.R

--- sandbox.R
+++ sandbox.R
@@ -52,3 +52,39 @@ printCMH <- function(x, vec) { # where x = outcome var, vec = pred. indices
 for (i in 2:4) {
   printCMH(mydata[, i], c(org_rel, emp_rel))
 }
+
+########################################################################
+library(effects)
+predictors <- c("risks.time", "risks.communication",
+                         "risks.lackemployeecontrol", "risks.unclearPolicy",
+                       "risks.workinghours")
+eff.obj1 <- effect("risks.time", ordered.fit)
+eff.obj2 <- effect("risks.communication", ordered.fit)
+eff.obj3 <- effect("risks.lackemployeecontrol", ordered.fit)
+eff.obj4 <- effect("risks.unclearPolicy", ordered.fit)
+eff.obj5<- effect("risks.workinghours", ordered.fit)
+plot(eff.obj1, main = FALSE, grid = TRUE, row = 1, col = 1, nrow = 1,
+     ncol = 3, more = TRUE)
+plot(eff.obj2, main = FALSE, grid = TRUE, ylab = "", row = 1, col = 2,
+     nrow = 1, ncol = 3, more = TRUE)
+plot(eff.obj3, main = FALSE, grid = TRUE, ylab = "", row = 1, col = 3, 
+     nrow = 1, ncol = 3, more = FALSE)
+
+plot(eff.obj4, main = FALSE, grid = TRUE, row = 1, col = 1, 
+     nrow = 1, ncol = 3, more = TRUE)
+plot(eff.obj5, main = FALSE, grid = TRUE, ylab = "", row = 1, col = 2, 
+     nrow = 1, ncol = 3, more = FALSE)
+
+
+
+###########################################################################
+# draw a formatted table
+library(rtf)
+output <- "tables.doc"
+rtf <- RTF(output, width = 8.5, height = 11, font.size = 11, omi = c(1, 1, 1, 1))
+flat <- as.data.frame(flat) %>%
+  spread(concern.stress, Freq)
+head(flat)
+colnames(flat) <- gsub("\\.", " ", colnames(flat))
+addTable(rtf, flat, font.size = 9, row.names = FALSE, NA.string = "-")
+done(rtf)
