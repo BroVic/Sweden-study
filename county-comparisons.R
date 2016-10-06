e5d14b02bd5f2b45903231598a887b271f80255a
BroVic <victorordu@outlook.com>
2016-10-06 02:31:59 +0100

    Conduct various statistical tests
    After tabulating the data on country and concerns about stress (as an example), statistical test to assess degrees of independence and/or association where were equally carried.

--- county-comparisons.R
+++ county-comparisons.R
@@ -4,10 +4,26 @@ library(ggplot2)
 alldata <- readRDS("clean_x.rds")
 
 levels(alldata$country)
-alldata$country <- factor(alldata$country, levels = names(sort(table(alldata$country), decreasing = TRUE)))
+alldata$country <- factor(alldata$country,
+                          levels = names(sort(table(alldata$country),
+                                              decreasing = TRUE)))
 levels(alldata$country)
+
+country_table <- table(alldata$country, alldata$concern.stress)
+country_table
+country_proptab <- round(prop.table(country_table, margin = 1), digits = 2)
+country_proptab
+country_margtabRow <- margin.table(country_table, margin = 1)
+country_margtabRow
+country_margtabCol <- margin.table(country_table, margin = 2)
+country_margtabCol 
 
 countryPlot <- ggplot(alldata, aes(x = country, fill = concern.stress)) +
   geom_bar() +
   ggtitle("Distribution of countries")
 countryPlot
+
+vcdExtra::CMHtest(country_table)
+coin::cmh_test(country_table)
+vcdExtra::GKgamma(country_table)
+
