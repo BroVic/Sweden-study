e57754997fb46d81a975bcf459f516a219a12984
BroVic <victorordu@outlook.com>
2016-10-11 13:58:43 +0100

    Plot country against concerns over bullying and violence
    Two more plots were added to compare the concerns over bullying and violence in different EU countries.

--- county-comparisons.R
+++ county-comparisons.R
@@ -18,10 +18,23 @@ country_margtabRow
 country_margtabCol <- margin.table(country_table, margin = 2)
 country_margtabCol 
 
-countryPlot <- ggplot(alldata, aes(x = country, fill = concern.stress)) +
+
+countryPlot_stress <- ggplot(alldata, aes(x = country, fill = concern.stress)) +
   geom_bar() +
-  ggtitle("Distribution of countries")
-countryPlot
+  ggtitle("Distribution of countries vs. stress")
+countryPlot_stress
+
+countryPlot_bullying <- 
+  ggplot(alldata, aes(x = country, fill = concern.bullying)) +
+  geom_bar() +
+  ggtitle("Distribution of countries vs. bullying")
+countryPlot_bullying
+
+countryPlot_violence <-
+  ggplot(alldata, aes(x = country, fill = concern.violence)) +
+  geom_bar() +
+  ggtitle("Distribution of countries vs. violence")
+countryPlot_violence
 
 vcdExtra::CMHtest(country_table) # Cochrane-Mantel-Haenzl test
 coin::cmh_test(country_table)   # same but focusing on general associaion
