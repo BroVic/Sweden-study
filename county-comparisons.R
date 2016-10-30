b16033bb718d926033b971f07650041ffb9c19b8
BroVic <victorordu@outlook.com>
2016-10-30 16:31:38 +0100

    Explore comparisons between Skandinavia and other EU countries
    The 4 Skandinavian countries in the dataset (Denmark, Finland, Norway and Sweden) were compared with other EU countries and amongs themselves for concerns over stress, bullying and violence in the workplace. At this stage, only exploratory analysis has been done.

--- county-comparisons.R
+++ county-comparisons.R
@@ -1,5 +1,6 @@
 # country-comparisons
 library(ggplot2)
+library(vcdExtra)
 
 alldata <- readRDS("clean_x.rds")
 
@@ -13,9 +14,9 @@ country_table <- table(alldata$country, alldata$concern.stress)
 country_table
 country_proptab <- round(prop.table(country_table, margin = 1), digits = 2)
 country_proptab
-country_margtabRow <- margin.table(country_table, margin = 1)
+country_margtabRow <- margin.table(country_table, 1)
 country_margtabRow
-country_margtabCol <- margin.table(country_table, margin = 2)
+country_margtabCol <- margin.table(country_table, 2)
 country_margtabCol 
 
 
@@ -36,7 +37,44 @@ countryPlot_violence <-
   ggtitle("Distribution of countries vs. violence")
 countryPlot_violence
 
-vcdExtra::CMHtest(country_table) # Cochrane-Mantel-Haenzl test
+CMHtest(country_table) # Cochrane-Mantel-Haenzl test
 coin::cmh_test(country_table)   # same but focusing on general associaion
-vcdExtra::GKgamma(country_table) # Goodman-Kruskal statistic
+GKgamma(country_table) # Goodman-Kruskal statistic
 
+# To do:
+# 1. Analyse Skandinavian countries - Denmark, Norway, Sweden (Faroe Islands,
+#    Finland, Iceland)
+
+## create a new variables for Skandinavia
+alldata$isSkandinavian <- grepl("FI|DK|NO|SE", alldata$country)
+(tab1 <-table(alldata$concern.stress, alldata$isSkandinavian))
+round(prop.table(tab1, 2), 2)
+barplot(prop.table(tab1, 2))
+
+
+alldata$skand_OTHERS <- gsub("FI|DK|NO|SE", "SKND", alldata$country)
+theCountries <- levels(as.factor(alldata$skand_OTHERS))
+theCountries <- theCountries[-grep("SKND", theCountries)]
+theCountries <- c(theCountries, "SKND")
+alldata$skand_OTHERS <- factor(alldata$skand_OTHERS, levels = theCountries)
+str(alldata$skand_OTHERS)
+levels(alldata$skand_OTHERS)
+
+tab2 <- table(alldata$concern.bullying, alldata$skand_OTHERS)
+round(prop.table(tab2, 2), 2)
+barplot(prop.table(tab2, 2))   # nothing unusual seen
+
+# check out Skandinavia data
+Skandinavia_data <- alldata[alldata$isSkandinavian == T, ]
+Skandinavia_data <- droplevels(Skandinavia_data)
+dim(Skandinavia_data)
+
+summary(Skandinavia_data)
+(tab_stress <- table(Skandinavia_data$concern.stress, Skandinavia_data$country))
+barplot(tab_stress)
+(tab_bullying <- table(Skandinavia_data$concern.bullying,
+                      Skandinavia_data$country))
+barplot(tab_bullying)
+(tab_violence <- table(Skandinavia_data$concern.stress,
+                       Skandinavia_data$country))
+barplot(tab_violence)
