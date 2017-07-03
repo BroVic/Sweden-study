137f16831ce8dde4598ab6576de401bc16c16260
Victor Ordu <victorordu@outlook.com>
2017-07-03 04:24:56 +0100

    Changes file name

--- analysis/univariate.R
+++ analysis/univariate.R
@@ -1,27 +1,33 @@
 # Exploratory Data Analysis
 library(dplyr)
-library(tidyr)
+library(tibble)
 
 # Load the data
-eurodat <- readRDS("clean_2.rds")
+eurodat <- readRDS("data-cleaning/clean_2.rds")
 
 # generate overview of data table
-tbl_df(data = eurodat)
+glimpse(eurodat)
 summary(eurodat)
 
 
 # subset categorical variables
 categoricals <-  eurodat %>%
-  select(-id, -femaleEmployees, -over50)
+  select(-est_wei1, -est_wei2, -emp_wei1, -emp_wei2)
+
+numericals <- eurodat %>%
+  select(est_wei1, est_wei2, emp_wei1, emp_wei2)
 
 # print out univariate tables of all categorical variables in console
 sapply(categoricals, table)
 
 
 # print out bar charts of all the categorical variables in the dataset
-for (i in 1:96) {
+for (i in 1:length(tablesList)) {
   tab <- table(categoricals[, i])
   ncol <- colnames(categoricals)
   barplot(tab, main = paste("Barplot of ", ncol[i]))
 }
 
+summary(numericals)
+# generic plots for numerical data
+apply(numericals, 2, function(x) plot(x))
