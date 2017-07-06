70494dea5263581331b2726bbdb51adb69b2e077
Victor Ordu <victorordu@outlook.com>
2017-07-06 02:29:13 +0100

    Adds some more univariate plots

--- analysis/univariate.R
+++ analysis/univariate.R
@@ -13,21 +13,37 @@ summary(eurodat)
 # subset categorical variables
 categoricals <-  eurodat %>%
   select(-est_wei1, -est_wei2, -emp_wei1, -emp_wei2)
+glimpse(categoricals)
 
 numericals <- eurodat %>%
-  select(est_wei1, est_wei2, emp_wei1, emp_wei2)
+  select(id, est_wei1, est_wei2, emp_wei1, emp_wei2)
+glimpse(numericals)
 
 # print out univariate tables of all categorical variables in console
-sapply(categoricals, table)
+tablesList <- sapply(categoricals, table)
 
 
 # print out bar charts of all the categorical variables in the dataset
-for (i in 1:length(tablesList)) {
+for (i in 2:length(tablesList)) {
   tab <- table(categoricals[, i])
-  ncol <- colnames(categoricals)
-  barplot(tab, main = paste("Barplot of ", ncol[i]))
+  name_col <- colnames(categoricals)
+  barplot(tab,
+          main = paste("Barplot of", name_col[i]),
+          sub = paste("Variable label:", comment(categoricals[, i])))
 }
 
 summary(numericals)
-# generic plots for numerical data
+
+# generate various kinds of plots for numerical data
 apply(numericals, 2, function(x) plot(x))
+
+allCols <- 2:ncol(numericals)
+for (i in allCols)
+  boxplot(numericals[, i])
+
+for (i in allCols) {
+  hist(numericals[, i],
+       main = paste("Histogram of", colnames(numericals)[i]),
+       sub = paste("Variable label", comment(numericals[, i]))
+  )
+}
